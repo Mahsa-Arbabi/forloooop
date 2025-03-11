4761d63cfeae8dd78f213ad3271e887e231e38c3
Mahi <mimarbabi80@gmail.com>
2025-03-12 02:30:28 +0330

    changes

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 const arr = [1, 2, 5, 7, 10];
 for (let i = 0; i < arr.length; i++) {
   for (let j = 1; j <= i; j++) {
-    if (arr[i] + arr[j] === 9) 
+    if (arr[i] + arr[j] === 9) {
         console.log("truuuuuuue");
+        let b = arr.indexOf(arr[i]);
+        let c = arr.indexOf(arr[j]);
+
+      console.log("["+b +","+ c+"]");
+      }
   }
 }
